c28397b9cdf8d7093453e6fd8d8f2bb4c8918e9e
eduvisiontrainer <40383284+eduvisiontrainer@users.noreply.github.com>
2019-12-22 11:06:21 +0100

    Updated config

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
   title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
+  author: 'Jim Dunk', // Required
   description: 'Full-stack Web Developer',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    website: 'https://unboundit.com',
+    github: 'https://github.com/jdunk',
+    twitter: 'https://twitter.com/jdunk',
+    linkedin: 'https://nl.linkedin.com/in/jdunk',
   },
   pathPrefix: '/devblog',
-  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+  siteUrl: 'https://nostalgic-ptolemy-af840d.netlify.com/devblog/',
 };
